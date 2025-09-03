753aab72cbb36a77778b55f23884aebfbc3dff4c
Mahdi Hosseini <mahdihosseinidev@gmail.com>
2025-09-04 00:45:49 +0330

    add lesson 2

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+# Operators
+
+# Arithmetic
+
+# Assignment
+data = 30
+data = data + 3
+data += 3
+print(data)
+
+
+# comparison
+
+first_var = 30
+sec_var = "30"
+print(first_var == sec_var)
+print(first_var > sec_var)
+print(first_var < sec_var)
+print(first_var != sec_var)
+print(first_var >= sec_var)
+print(first_var <= sec_var)
